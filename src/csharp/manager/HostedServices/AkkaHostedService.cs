778886a5e976120d2de150030012973d687320e0
심승용 <seungyong.shim@mirero.co.kr>
2020-10-29 23:14:15 +0900

    커밋

---
+++ src/csharp/manager/HostedServices/AkkaHostedService.cs
@@ -0,0 +1,32 @@
+﻿using Akka.Actor;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace manager
+{
+    internal class AkkaHostedService : IHostedService
+    {
+        public AkkaHostedService(ActorSystem actorSystem, ILogger<AkkaHostedService> logger)
+        {
+            ActorSystem = actorSystem;
+            Logger = logger;
+        }
+
+        public ActorSystem ActorSystem { get; }
+        public ILogger<AkkaHostedService> Logger { get; }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var serviceRootActor = ActorSystem.ActorOf(Props.Create(() => new ServiceRootActor()), nameof(ServiceRootActor));
+
+            await Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/csharp/manager/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace manager
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Host.CreateDefaultBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton(sp => Akka.Actor.ActorSystem.Create("mls.inference.manager"));
+                    services.AddHostedService<AkkaHostedService>();
+                })
+                .Build()
+                .RunAsync();
+        }
+    }
+}
